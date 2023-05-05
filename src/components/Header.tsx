2c2393a1c224f61dcdaf34ce6e953393cfa4f7bb
PCGAMER-DIANA\Diana <diana.fp.gomes@gmail.com>
2023-05-05 19:44:29 +0100

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,40 @@
 import Link from 'next/link';
+import { MouseEvent, useState } from 'react';
 
 const Header = () => {
+  const [page, setPage] = useState(0);
+
+  const handleClick = (value: number) => {
+    setPage(value);
+  };
+
   return (
     <div>
       <nav>
-        <ul className="text-slate-100">
-          <li>
-            <Link href="/">Home</Link>
+        <ul className="flex text-base">
+          <li
+            onClick={() => handleClick(0)}
+            className={`${
+              page === 0 ? `text-fuchsia-800 ` : `text-white `
+            } && flex-1 mx-8 cursor-pointer hover:text-fuchsia-800 font-bold`}
+          >
+            <Link href="/">HOME</Link>
           </li>
-          <li>
-            <Link href="/about">About</Link>
+          <li
+            onClick={() => handleClick(1)}
+            className={`${
+              page === 1 ? `text-fuchsia-800 ` : `text-white `
+            } && flex-1 mx-8 cursor-pointer hover:text-fuchsia-800 font-bold`}
+          >
+            <Link href="/about">ABOUT</Link>
           </li>
-          <li>
-            <Link href="/projects">Projects</Link>
+          <li
+            onClick={() => handleClick(2)}
+            className={`${
+              page === 2 ? `text-fuchsia-800 ` : `text-white `
+            } && flex-1 mx-8 cursor-pointer hover:text-fuchsia-800 font-bold`}
+          >
+            <Link href="/projects">PROJECTS</Link>
           </li>
         </ul>
       </nav>
